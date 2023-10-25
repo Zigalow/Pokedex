665a59e298694434e474c63a895bf02321476aa1
zahed wafa <s224316@dtu.dk>
2023-10-25 23:39:53 +0200

    SettingsPage under udvikling

--- app/src/main/java/dtu/group21/pokedex/MainActivity.kt
+++ app/src/main/java/dtu/group21/pokedex/MainActivity.kt
@@ -1,15 +1,40 @@
-package dtu.group21.pokedex
+package com.example.udviklingafsettings
 
+import android.graphics.Color
+import android.graphics.ColorSpace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.colorspace.ColorSpaces
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import com.example.group21.ui.theme.PokedexTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +42,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PokedexTheme {
-                // A surface container using the 'background' color from the theme
+                // A surface container us3ing the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -28,6 +53,76 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SettingsPage() {
+    var searchText by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+
+
+        Text(
+            text = "Settings",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 16.dp)
+        )
+
+        TextField(
+            value = searchText,
+            onValueChange = { searchText = it },
+            label = { Text("Search") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .align(Alignment.CenterHorizontally),
+
+        )
+
+        Button(
+            onClick = {/* Do something */ },
+            modifier = Modifier
+                .padding(top = 8.dp)
+                .fillMaxWidth()
+                .align(Alignment.Start),
+
+        ) {
+            Text(text = "Dark mode")
+        }
+        Button(
+            onClick = {/* Do something */ },
+            modifier = Modifier
+                .padding(top = 8.dp)
+                .fillMaxWidth()
+                .align(Alignment.Start)
+        ) {
+            Text(text = "Guide")
+        }
+        Button(
+            onClick = {/* Do something */ },
+            modifier = Modifier
+                .padding(top = 8.dp)
+                .fillMaxWidth()
+                .align(Alignment.Start)
+
+        ) {
+            Text(text = "About us")
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun SettingspagePreview(){
+    SettingsPage()
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
