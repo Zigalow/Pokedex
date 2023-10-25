4a440cec1f1fe112973777d06f671aefd35a36d5
Zigalow <s224302@dtu.dk>
2023-10-25 04:41:51 +0200

    Updated to include hexclolors for regular use, and for background. Hardcoded the ones that should be relevant for hifi

--- app/src/main/java/dtu/group21/models/pokemon/PokemonType.kt
+++ app/src/main/java/dtu/group21/models/pokemon/PokemonType.kt
@@ -1,22 +1,25 @@
 package dtu.group21.models.pokemon
 
-enum class PokemonType {
-    NORMAL,
-    FIRE,
-    WATER,
-    ELECTRIC,
-    GRASS,
-    ICE,
-    FIGHTING,
-    POISON,
-    GROUND,
-    FLYING,
-    PSYCHIC,
-    BUG,
-    ROCK,
-    GHOST,
-    DRAGON,
-    DARK,
-    STEEL,
-    FAIRY
+enum class PokemonType(val regularColorHexvalue: String, val backgroundColorHexvalue: String) {
+    NORMAL("d","d"),
+    FIRE("#FD7D24","#FFCFAD"),
+    WATER("#4592C4","#B6E2FF"),
+    ELECTRIC("d","d"),
+    GRASS("#9BCC50","#CBFCB4"),
+    ICE("d","d"),
+    FIGHTING("d","d"),
+    POISON("#B97FC9","d"),
+    GROUND("d","d"),
+    FLYING("#BDB9B8","d"),
+    PSYCHIC("d","d"),
+    BUG("d","d"),
+    ROCK("d","d"),
+    GHOST("d","d"),
+    DRAGON("d","d"),
+    DARK("d","d"),
+    STEEL("d","d"),
+    FAIRY("d","d");
+
+
+   
 }
