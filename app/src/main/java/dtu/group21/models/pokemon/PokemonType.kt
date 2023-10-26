1b22c83f60c8f63e44de8976bffb6fb426cf6008
Zigalow <s224302@dtu.dk>
2023-10-26 16:47:00 +0200

    updated pokemonType colors so that they're are of type Color and updated their hexvalues as well

--- app/src/main/java/dtu/group21/models/pokemon/PokemonType.kt
+++ app/src/main/java/dtu/group21/models/pokemon/PokemonType.kt
@@ -1,25 +1,26 @@
 package dtu.group21.models.pokemon
 
-enum class PokemonType(val regularColorHexvalue: String, val backgroundColorHexvalue: String) {
-    NORMAL("d","d"),
-    FIRE("#FD7D24","#FFCFAD"),
-    WATER("#4592C4","#B6E2FF"),
-    ELECTRIC("d","d"),
-    GRASS("#9BCC50","#CBFCB4"),
-    ICE("d","d"),
-    FIGHTING("d","d"),
-    POISON("#B97FC9","d"),
-    GROUND("d","d"),
-    FLYING("#A890F0","d"),
-    PSYCHIC("d","d"),
-    BUG("#729F3F","#DBFCB5"),
-    ROCK("d","d"),
-    GHOST("d","d"),
-    DRAGON("d","d"),
-    DARK("d","d"),
-    STEEL("d","d"),
-    FAIRY("d","d");
+import androidx.compose.ui.graphics.Color
+
+enum class PokemonType(val primaryColor: Color, val secondaryColor: Color) {
+    NORMAL(Color(0xFF000000), Color(0xFF000000)),
+    FIRE(Color(0xFFFD7D24), Color(0xFFFFCFAD)),
+    WATER(Color(0xFF4592C4), Color(0xFFB6E2FF)),
+    ELECTRIC(Color(0xFF000000), Color(0xFF000000)),
+    GRASS(Color(0xFF9BCC50), Color(0xFFCBFCB4)),
+    ICE(Color(0xFF000000), Color(0xFF000000)),
+    FIGHTING(Color(0xFF000000), Color(0xFF000000)),
+    POISON(Color(0xFFB97FC9), Color(0xFF000000)),
+    GROUND(Color(0xFF000000), Color(0xFF000000)),
+    FLYING(Color(0xFFA890F0), Color(0xFF000000)),
+    PSYCHIC(Color(0xFF000000), Color(0xFF000000)),
+    BUG(Color(0xFF729F3F), Color(0xFFDBFCB5)),
+    ROCK(Color(0xFF000000), Color(0xFF000000)),
+    GHOST(Color(0xFF000000), Color(0xFF000000)),
+    DRAGON(Color(0xFF000000), Color(0xFF000000)),
+    DARK(Color(0xFF000000), Color(0xFF000000)),
+    STEEL(Color(0xFF000000), Color(0xFF000000)),
+    FAIRY(Color(0xFF000000), Color(0xFF000000));
 
 
-   
 }
