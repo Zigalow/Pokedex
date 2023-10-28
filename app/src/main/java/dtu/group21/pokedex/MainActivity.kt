690916e031c61c32f0ffb919957d9e3271a1bd6f
Zigalow <s224302@dtu.dk>
2023-10-28 19:25:31 +0200

    Deleted unnecessary code

--- app/src/main/java/dtu/group21/pokedex/MainActivity.kt
+++ app/src/main/java/dtu/group21/pokedex/MainActivity.kt
@@ -1,33 +1,19 @@
 package dtu.group21.pokedex
 
-import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -46,21 +32,13 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.group21.ui.theme.PokedexTheme
 import com.example.pokedex.R
-import dtu.group21.models.pokemon.Pokemon
-import dtu.group21.models.pokemon.PokemonType
 
-    class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity() {
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContent {
                 PokedexTheme {
-                    // A surface container us3ing the 'background' color from the theme
-                    Surface(
-                        modifier = Modifier.fillMaxSize(),
-                        color = MaterialTheme.colorScheme.background
-                    ) {
-                        Greeting("Android")
-                    }
+                  SettingsPage()
                 }
             }
         }
@@ -169,18 +147,4 @@ import dtu.group21.models.pokemon.PokemonType
         SettingsPage()
     }
 
-    @Composable
-    fun Greeting(name: String, modifier: Modifier = Modifier) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-    }
-
-    @Preview(showBackground = true)
-    @Composable
-    fun GreetingPreview() {
-        PokedexTheme {
-            Greeting("Android")
-        }
-    }
+ 
