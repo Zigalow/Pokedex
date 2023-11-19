0bc989e39748a0e39666c05d21188be9c253b308
Zigalow <s224302@dtu.dk>
2023-11-19 14:20:05 +0100

    Done with getSimplePokemon

--- app/src/main/java/dtu/group21/models/api/PokedexRequestMaker.kt
+++ app/src/main/java/dtu/group21/models/api/PokedexRequestMaker.kt
@@ -1,7 +1,31 @@
 package dtu.group21.models.api
 
+import dtu.group21.models.pokemon.SimplePokemon
+import java.util.jar.Attributes.Name
+
 class PokedexRequestMaker {
-    
-    
+
+    val apiURL = "https://pokeapi.co/api/v2"
+    val jsonRequestMaker: JsonRequestMaker = JsonRequestMaker(apiURL)
+
+    fun getSimplePokemon(pokedexId: Int): SimplePokemon {
+        val species = jsonRequestMaker.makeRequest("pokemon-species/$pokedexId")
+        val names = species.getJSONArray("names")
+        var name = ""
+
+        for (i in 0 until names.length()) {
+            val nameObject = names.getJSONObject(i)
+            val language = nameObject.getJSONObject("language").getString("name")
+
+            if (!language.equals("en")) {
+                continue;
+            }
+            name = nameObject.getString("name")
+        }
+
+        return SimplePokemon(pokedexId, name)
+
+    }
+
 
 }
