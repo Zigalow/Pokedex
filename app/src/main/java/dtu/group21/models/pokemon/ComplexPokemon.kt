b8acfa935a71b709ae1b31e4344e7b47c664dd13
Zigalow <s224302@dtu.dk>
2023-11-24 01:35:08 +0100

    added categoryName, abilities, weight and height for data class, and updated spriteResourceId to fetch picture from internet

--- app/src/main/java/dtu/group21/models/pokemon/ComplexPokemon.kt
+++ app/src/main/java/dtu/group21/models/pokemon/ComplexPokemon.kt
@@ -5,10 +5,16 @@ class ComplexPokemon(
     val type: PokemonType,
     val secondaryType: PokemonType,
     val gender: PokemonGender,
+    val categoryName: String,
+    val abilities: Array<PokemonAbility>,
+    val weightInGrams: Int,
+    val heightInCm: Int,
     val stats: PokemonStats,
     val species: PokemonSpecies,
     val moves: Array<PokemonMove>,
 ) {
-    val spriteResourceId = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id.toString().padStart(3, '0')}.png"
+    val spriteResourceId = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/${
+        id.toString().padStart(3, '0')
+    }.png"
     val hasTwoTypes = secondaryType != PokemonType.NONE
 }
