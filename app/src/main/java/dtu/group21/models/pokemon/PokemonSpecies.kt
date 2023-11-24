5bfd38085751d612c59bd57b77a69fc7ba4eae3c
Zigalow <s224302@dtu.dk>
2023-11-24 01:35:44 +0100

    Added genderRate

--- app/src/main/java/dtu/group21/models/pokemon/PokemonSpecies.kt
+++ app/src/main/java/dtu/group21/models/pokemon/PokemonSpecies.kt
@@ -2,6 +2,7 @@ package dtu.group21.models.pokemon
 
 data class PokemonSpecies(
     val name: String,
+    val genderRate: Int,
     val hasGenderDifferences: Boolean,
     val isBaby: Boolean,
     val isLegendary: Boolean,
