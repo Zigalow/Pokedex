e8dac5ee2d84aa49ac038b39b2d37a08c3dcf72c
Zigalow <s224302@dtu.dk>
2023-11-26 23:03:01 +0100

    Saves locally to database again even when closing app

--- app/src/main/java/dtu/group21/pokedex/MainActivity.kt
+++ app/src/main/java/dtu/group21/pokedex/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
         database = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "pokedex-database").build()
 
         setContent {
