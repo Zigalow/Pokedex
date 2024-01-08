7d44b13d31246aa25b3c1d155bfdb2a7ded3ed4b
Zigalow <s224302@dtu.dk>
2024-01-08 12:00:15 +0100

    Done with filtering

--- app/src/main/java/dtu/group21/ui/search/FilterSettings.kt
+++ app/src/main/java/dtu/group21/ui/search/FilterSettings.kt
@@ -3,12 +3,23 @@ package dtu.group21.ui.search
 class FilterSettings {
     enum class FilterType {
         IncludableTypes,
-        DualType,
+        ExactType,
     }
 
     var filterType = FilterType.IncludableTypes
     val types: ArrayList<Boolean> = arrayListOf(false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false)
 
+
+    fun numberOfTypesChosen(): Int {
+        var counter = 0
+        for (i in types) {
+            if (i) {
+                counter++
+            } 
+        }
+        return counter
+    }
+    
     fun reset() {
         filterType = FilterType.IncludableTypes
         for (i in 0 until types.size) {
