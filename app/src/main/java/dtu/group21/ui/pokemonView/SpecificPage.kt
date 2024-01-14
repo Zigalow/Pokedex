9fdb86f5d3212dc661fce470dee1599aff3f5042
Zigalow <s224302@dtu.dk>
2024-01-14 22:46:07 +0100

    Added CategoryList to MoveSection

--- app/src/main/java/dtu/group21/ui/pokemonView/SpecificPage.kt
+++ app/src/main/java/dtu/group21/ui/pokemonView/SpecificPage.kt
@@ -49,10 +49,10 @@ import coil.compose.rememberAsyncImagePainter
 import com.example.pokedex.R
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dtu.group21.data.PokedexViewModel
-import dtu.group21.models.api.PokemonViewModel
 import dtu.group21.data.database.DatabaseViewModel
-import dtu.group21.models.pokemon.ComplexPokemon
 import dtu.group21.data.pokemon.DetailedPokemon
+import dtu.group21.models.api.PokemonViewModel
+import dtu.group21.models.pokemon.ComplexPokemon
 import dtu.group21.models.pokemon.EvolutionChainPokemon
 import dtu.group21.models.pokemon.PokemonGender
 import dtu.group21.models.pokemon.PokemonMove
@@ -64,7 +64,6 @@ import dtu.group21.ui.frontpage.PokemonImage
 import dtu.group21.ui.frontpage.capitalizeFirstLetter
 import dtu.group21.ui.frontpage.formatPokemonId
 import dtu.group21.ui.theme.LightWhite
-
 
 @Composable
 fun SpecificPage(pokedexId: Int, onNavigateBack: () -> Unit) {
@@ -141,36 +140,36 @@ fun Top(
             modifier = Modifier
                 .weight(1f)
         ) {
-        backIcon(
-            modifier
-                .padding(vertical = 16.dp, horizontal = 19.dp)
-                .size(49.dp)
-                .clickable { onClickBack() }
-        )
+            backIcon(
+                modifier
+                    .padding(vertical = 16.dp, horizontal = 19.dp)
+                    .size(49.dp)
+                    .clickable { onClickBack() }
+            )
         }
         //Spacer(modifier.width(230.dp))
         Box(
             modifier = Modifier
                 .weight(0.1f)
         ) {
-        FavoritesIcon(
-            active = pokemon.isFavorite.value,
-            color = pokemon.primaryType.secondaryColor,
-            onClicked = {
-                pokemon.isFavorite.value = !pokemon.isFavorite.value
-                val database = MainActivity.database!!
-                val databaseViewModel = DatabaseViewModel()
-                val saveablePokemon = (pokemon as ComplexPokemon) // TODO
-                if (pokemon.isFavorite.value) {
-                    println("Saving pokemon")
-                    databaseViewModel.insertPokemon(saveablePokemon, database)
-                } else {
-                    println("Deleting pokemon")
-                    databaseViewModel.deletePokemon(saveablePokemon, database)
+            FavoritesIcon(
+                active = pokemon.isFavorite.value,
+                color = pokemon.primaryType.secondaryColor,
+                onClicked = {
+                    pokemon.isFavorite.value = !pokemon.isFavorite.value
+                    val database = MainActivity.database!!
+                    val databaseViewModel = DatabaseViewModel()
+                    val saveablePokemon = (pokemon as ComplexPokemon) // TODO
+                    if (pokemon.isFavorite.value) {
+                        println("Saving pokemon")
+                        databaseViewModel.insertPokemon(saveablePokemon, database)
+                    } else {
+                        println("Deleting pokemon")
+                        databaseViewModel.deletePokemon(saveablePokemon, database)
+                    }
                 }
-            }
 
-        )
+            )
         }
         Spacer(modifier.width(11.dp))
     }
@@ -257,10 +256,12 @@ fun Bottom(modifier: Modifier = Modifier, pokemon: DetailedPokemon) {
                 .width(13.dp)
                 .height(25.dp)
         )
-        Box( modifier = Modifier
-            //.padding(horizontal = 5.dp)
-            .fillMaxWidth()
-            .wrapContentHeight()) {
+        Box(
+            modifier = Modifier
+                //.padding(horizontal = 5.dp)
+                .fillMaxWidth()
+                .wrapContentHeight()
+        ) {
             CategoryList(
                 categories = categories,
                 onCategorySelected = { selectedCategory = it },
@@ -278,7 +279,8 @@ fun Bottom(modifier: Modifier = Modifier, pokemon: DetailedPokemon) {
             Sections(selectedCategory = selectedCategory, pokemon = pokemon, modifier = modifier)
             Spacer(
                 modifier = Modifier
-                    .weight(1f))
+                    .weight(1f)
+            )
         }
     }
 }
@@ -332,7 +334,6 @@ fun CategoryList(
     }
     onCategorySelected(selectedCategory)
 }
-
 
 @Composable
 fun Table(first: String, second: String) {
@@ -439,8 +440,34 @@ fun StatsSection(
 fun MovesSection(
     moves: Array<PokemonMove>
 ) {
-    Column {
-        MoveBoxColumn(moveList = moves.toList())
+    val moveCategories = listOf("Level", "Machine", "Egg", "Tutor")
+    var selectedCategory by remember { mutableStateOf("Level") }
+    Column(
+        modifier = Modifier
+            //.padding(horizontal = 5.dp)
+            .fillMaxWidth()
+            .wrapContentHeight()
+    )
+    {
+
+        Box(
+            modifier = Modifier
+                .height(25.dp)
+                .align(Alignment.CenterHorizontally),
+            ) {
+            Text(text = "Move categories", modifier = Modifier.align(Alignment.Center))
+        }
+        
+        CategoryList(
+            categories = moveCategories,
+            onCategorySelected = { selectedCategory = it },
+            initiallyChosen = selectedCategory,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+
+        Column {
+            MoveBoxColumn(moveList = moves.toList())
+        }
     }
 }
 
@@ -517,7 +544,6 @@ fun EvolutionPokemonImage(modifier: Modifier = Modifier, pokemon: EvolutionChain
     )
 }
 
-
 //region main components
 
 /*@Composable
@@ -568,7 +594,6 @@ fun FavoritesIcon(
     }
 }
 
-
 @Composable
 fun backIcon(modifier: Modifier) {
     Image(
@@ -586,7 +611,6 @@ fun arrow(modifier: Modifier) {
         modifier = modifier
     )
 }
-
 
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
@@ -638,7 +662,6 @@ fun MoveBoxColumn(moveList: List<PokemonMove>) {
         maxItemsInEachRow = 2
     ) {
 
-
         Spacer(modifier = Modifier.padding(vertical = 5.dp))
         for (i in moveList.indices) {
             moveBox(
@@ -652,7 +675,6 @@ fun MoveBoxColumn(moveList: List<PokemonMove>) {
         }
     }
 }
-
 
 @Composable
 fun moveBox(move: PokemonMove) {
@@ -746,6 +768,7 @@ fun moveBox(move: PokemonMove) {
         }
     }
 }
+
 @Composable
 fun LargerPokemonTypeBox(modifier: Modifier = Modifier, pokemonType: PokemonType) {
     Box(
